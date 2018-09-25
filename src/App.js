c23aaed6ecf29cb98242f32f5758d1db92ebf785
AssetsC <43584336+AssetsC@users.noreply.github.com>
2018-09-26 01:06:36 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import { BrowserQRCodeReader} from '@zxing/library';
+import swal from 'sweetalert2';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.cameraScanner = this.cameraScanner.bind(this);
+    
+    this.cameraScanner();
+  }
+
+  cameraScanner(){
+      const codeReader = new BrowserQRCodeReader();
+      codeReader.getVideoInputDevices().then(videoInputDevices => {
+
+      const firstDeviceId = videoInputDevices[0].deviceId;
+      codeReader.decodeFromInputVideoDevice(firstDeviceId, 'video')
+      .then(result => {
+            let res = result.text;
+
+            if(/material\/description/.test(res)){
+              swal({
+                type: 'success',
+                title: 'ทำการสแกนเรียบร้อย',
+                text: "กรุณากดปุ่ม ตกลง เพื่อเข้าหน้าอุปกรณ์",
+                confirmButtonText: "ตกลง"
+              }).then(data=>{
+                  if(data.value)
+                    window.location.href = res;
+              });
+            }
+            
+      }).catch(err => console.error(err));
+    }).catch(err => console.error(err));
+  }
+
+  render() {
+    return (
+      <div>
+          <h1 style={style.center}>Asset Scanner</h1>
+          <div style={style.camera}> 
+              <video id="video" style={{"margin":"auto","borderRadius":"30px","padding" : "15px","boxShadow":"0px 0px 3px grey"}}></video>
+          </div>    
+      </div>
+    );
+  }
+}
+
+const style = {
+    "center" : {
+      "textAlign":"center"
+    },
+    "camera" : {
+      "display":"flex",
+      "width" : "100%"
+    }
+}
+
+export default App;
